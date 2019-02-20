6edd1ef4686f6d6c19372d21eb2c86cf03cdf0af
= <=>
2019-02-20 11:42:34 -0600

    Configurando vista para tags, generando demos para tags

--- __manifest__.py
+++ __manifest__.py
@@ -27,6 +27,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/Tags.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
